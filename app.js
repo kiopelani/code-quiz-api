a962711a9e433231360f9f7ca774a35bdbfe4528
Kiope Gyzen <kiopelani@gmail.com>
2015-03-10 16:57:16 -0700

    Required CORS

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ var app = express();
 
 app.use(express.static('public'));
 
+var cors = require('cors');
+
+app.use(cors());
+
 var questions = require('./routes/questions');
 app.use('/api/v1/questions', questions);
 
-app.all('/*', function(request, response, next){
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
-  response.header('Access-Control-Allow-Headers', 'Content-type', 'Accept');
-  next();
-});
+// app.all('/*', function(request, response, next){
+//   response.header("Access-Control-Allow-Origin", "*");
+//   response.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+//   response.header('Access-Control-Allow-Headers', 'Content-type', 'Accept');
+//   next();
+// });
 
 app.get('/', function(done){
   response.status(200);
