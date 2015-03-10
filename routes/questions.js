24f91d47d721b666f05e4ce8f3c832763e5ec31e
Kiope Gyzen <kiopelani@gmail.com>
2015-03-10 16:15:18 -0700

    No select for production

--- routes/questions.js
+++ routes/questions.js
@@ -11,9 +11,9 @@ if(process.env.REDISTOGO_URL){
   client.auth(rtg.auth.split(":")[1]);
 } else {
   var client = redis.createClient();
+  client.select((process.env.NODE_ENV || 'development').length);
 }
 
-client.select((process.env.NODE_ENV || 'development').length);
 
 var router = express.Router();
 //END REDIS CONNECTION
