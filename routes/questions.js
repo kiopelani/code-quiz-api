a0e4e6d2ac1de1f274a3b0062a6bacdce2949cc4
Kiope Gyzen <kiopelani@gmail.com>
2015-03-10 16:12:27 -0700

    Enable redis in production

--- routes/questions.js
+++ routes/questions.js
@@ -5,7 +5,13 @@ var urlencode = bodyParser.urlencoded({ extended: false });
 
 //BEGIN REDIS CONNECTION
 var redis = require('redis');
-var client = redis.createClient();
+if(process.env.REDISTOGO_URL){
+  var rtg = require("url").parse(process.env.REDISTOGO_URL);
+  var client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+  var client = redis.createClient();
+}
 
 client.select((process.env.NODE_ENV || 'development').length);
 
